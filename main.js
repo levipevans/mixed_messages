2e7ae81e300421a201df50ae886d07422f5f36e1
Levi Evans <levipe16@gmail.com>
2021-02-18 05:50:42 -0800

    function sorts through nested arrays !work

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-const randomJoke = (arr1, arr2, arr3) =>{
-    
+const jokeStart = ["1","2","3"];
+const jokeMid = ["4","5","6"];
+const jokeEnd = ["7","8","9"];
+
+const jokeStash =[jokeStart,jokeMid, jokeEnd];
+
+const randomMess = (stash) =>{
+    let randomMessage = "";
+    for(i in stash){
+        let rand =Math.floor( Math.random * stash[i].length);
+        randomMessage += stash[i][rand];
+    }
+    return randomMessage;
 }
+console.log(randomMess(jokeStash));
